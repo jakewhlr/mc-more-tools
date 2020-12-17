1188ba8880235b4d15e1002c27470fe0503ddad8
Jake Wheeler <jakewhlr@gmail.com>
2020-12-17 00:36:25 -0800

    Delete copy.sh

--- src/main/resources/assets/more_tools/models/copy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-for TOOL in "axe" "hoe" "pickaxe" "shovel" "sword"
-do
-    for MATERIAL in "birch" "crimson" "dark_oak" "jungle" "oak" "warped"
-    do
-        sed -i 's/$MATERIAL/'"$MATERIAL"'/g' ${MATERIAL}_${TOOL}.json
-    done
-done
