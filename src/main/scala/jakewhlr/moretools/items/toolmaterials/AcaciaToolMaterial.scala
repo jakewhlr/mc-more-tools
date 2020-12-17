87b72c58e19f3fe3d5c4557bee10f5c6b072d30b
Jake Wheeler <jakewhlr@gmail.com>
2020-12-16 19:40:58 -0800

    added recipes for acacia and spruce tools

--- src/main/scala/jakewhlr/moretools/items/toolmaterials/AcaciaToolMaterial.scala
+++ src/main/scala/jakewhlr/moretools/items/toolmaterials/AcaciaToolMaterial.scala
@@ -4,10 +4,11 @@ import net.minecraft.item.{Items, ToolMaterial}
 import net.minecraft.recipe.Ingredient
 
 object AcaciaToolMaterial extends ToolMaterial{
+  def craftingIngredient: Ingredient = Ingredient.ofItems(Items.ACACIA_PLANKS)
   override def getDurability: Int = 59
   override def getMiningSpeedMultiplier: Float = 1
   override def getAttackDamage: Float = 1
   override def getMiningLevel: Int = 1
   override def getEnchantability: Int = 15
-  override def getRepairIngredient: Ingredient = Ingredient.ofItems(Items.ACACIA_PLANKS)
+  override def getRepairIngredient: Ingredient = craftingIngredient
 }
